3850b8d8dded88e0d7acba38a6094e07ea06d218
ryan <ryanmccombe@gmail.com>
2019-04-20 23:09:36 +0100

    Routines, channels

---
+++ 9_goConcurrency/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+	links := []string {
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
+		"http://amazon.com",
+	}
+
+	channel := make(chan string)
+
+	for _, link := range links {
+		go checkLink(link, channel)
+	}
+
+	for link := range channel {
+		go func(link string) {
+			time.Sleep(5 * time.Second)
+			checkLink(link, channel)
+		}(link)
+	}
+}
+
+func checkLink(link string, channel chan string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "might be down!")
+		channel <- link
+		return
+	}
+
+	channel <- link
+	fmt.Println(link, "is up!")
+}
